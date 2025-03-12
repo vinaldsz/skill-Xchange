4a4cb1372d9992c9ea3cfc39cbf2c5cb0afc010b
John Alexis Guerra Gómez <john.guerra@gmail.com>
2025-03-12 10:16:52 -0700

    Feb 28 changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,15 @@
+import ProjectsList from "./components/ProjectsList.jsx";
+
+import "./App.css";
+
 export default function App() {
-  return <div>Hola Yuxin</div>;
+  return (
+    <div className="App">
+      <div>Projects</div>
+
+      <ProjectsList />
+
+
+    </div>
+  );
 }
